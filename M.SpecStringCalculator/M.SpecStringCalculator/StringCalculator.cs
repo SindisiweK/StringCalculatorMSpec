468a938c6f3c57f2551b9b9528d4fe2f72ba6faf
SindisiweK <SindisiweK@sahomeloans.com>
2018-08-24 11:10:27 +0200

    string calculator mspec solution

---
+++ M.SpecStringCalculator/M.SpecStringCalculator/StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace M.SpecStringCalculator
+{
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+        }
+
+        public int Add(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+
+            var separator = new char[] { ',', '\n', ';', '*','^','#','@','!','&',':','?','|','%','$','~','`','.','<','>','_','+','='};
+            var output = input
+                .Replace(")",string.Empty)
+                .Replace("(", string.Empty)
+                .Replace("}",string.Empty)
+                .Replace("{",string.Empty)
+                .Replace("]", string.Empty)
+                .Replace("[", string.Empty)
+                .Replace("//", string.Empty)
+                .Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            var negativeValues = output.Where(x => int.Parse(x) < 0);
+            if (negativeValues.Any())
+            {
+                throw new Exception($"Negatives are not allowed {input}");
+            }
+            return output.Where(x => int.Parse(x) < 1000).Sum(int.Parse);
+        }
+    }
+}
---
+++ M.SpecStringCalculator/M.SpecStringCalculator/When_Adding_InvalidInputOfEmptyString.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Machine.Specifications;
+using Machine.Specifications.Model;
+
+namespace M.SpecStringCalculator
+{
+    [Subject("Given invalid input")]
+    public class When_Adding_InvalidInputOfEmptyString
+    {
+        private Establish context = () =>
+        {
+            Subject = new StringCalculator();
+        };
+
+        private Because of = () => Actual = Subject.Add("");
+
+        private It Should_Return_Sum_Of_All_The_Input = () =>
+        {
+            Actual.Should().Be(0);
+        };
+
+        static StringCalculator Subject;
+        static int Actual;
+    }
+
+    [Subject("Given_Numbers_Separated_By_Delimeters")]
+    public class When_Adding_Numbers_Wth_Delimeters
+    {
+        private Establish context = () =>
+        {
+            Subject = new StringCalculator();
+        };
+
+        private Because of = () => Actual = Subject.Add("//;[*][%][?]\n1*2&3");
+
+        private It Should_Return_Sum_Of_All_The_Input = () =>
+        {
+            Actual.Should().Be(6);
+        };
+
+        static StringCalculator Subject;
+        static int Actual;
+    }
+
+    [Subject("Given Negative Values")]
+    public class When_Input_Contains_Negative_Values
+    {                                               
+        private Establish context = () =>
+        {
+            Subject = new StringCalculator();
+        };
+
+        private Because of = () => _exception = Catch.Exception(() => Subject.Add("//;-1"));
+
+        private It Should_Throw_An_Exception_With_Message = () => 
+        {
+            _exception.Message.Contains($"Negatives are not allowed{-1}");
+        };
+
+        static StringCalculator Subject;
+        static Exception _exception;
+    }
+}
